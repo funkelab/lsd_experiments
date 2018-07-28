f453710a06f103e72a94e44eabda8b7e8b0f0799
Jan Funke <jfunke@iri.upc.edu>
2018-07-28 15:20:51 -0400

    Add test net for cremi setup04

--- cremi/02_train/setup04/mknet.py
+++ cremi/02_train/setup04/mknet.py
@@ -74,5 +74,4 @@ def create_network(input_shape, name):
 if __name__ == "__main__":
 
     create_network((84, 268, 268), 'train_net')
-    # TODO: find largest test size
-    # create_network((84, 268, 268), 'test_net')
+    create_network((96, 484, 484), 'test_net')
