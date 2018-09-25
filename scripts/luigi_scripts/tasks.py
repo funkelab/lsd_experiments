41a3962fff549e2fa2a0473719382f216d2db060
Arlo Sheridan <sheridana@c04u01.int.janelia.org>
2018-09-25 13:49:15 -0400

    Recombine extract fragments and agglomerate into single task
    When we have separate tasks for extracting and agglomerating, we run
    into a problem because agglomeration starts as soon as the edges
    collection is created even though there is nothing in it yet, and
    therefore nothing to agglomerate. We tried to fix this by creating a
    separate collection 'complete' after extracting fragments blockwise.
    We then returned that rather than nodes in the extract fragments task to
    no avail. Here, we recombine the extract/agglomerate scripts and merge
    the tasks. Extraction and agglomeration now works, but evaluation
    doesn't - seems to not be able to find the db name...

--- scripts/luigi_scripts/tasks.py
+++ scripts/luigi_scripts/tasks.py
@@ -112,7 +112,6 @@ class ProcessTask(luigi.Task):
         #print('FINISHED PREDICTION')
 
     def output(self):
-        print(self.output_filename())
         return N5DatasetTarget(self.output_filename(), 'volumes/' + self.predict_type)
 
     def output_dir(self):
@@ -121,7 +120,7 @@ class ProcessTask(luigi.Task):
     def output_filename(self):
         return os.path.join(self.output_dir(), self.sample)
 
-class ExtractFragments(luigi.Task):
+class ExtractAgglomerate(luigi.Task):
 
     experiment = luigi.Parameter()
     setup = luigi.Parameter()
@@ -129,8 +128,6 @@ class ExtractFragments(luigi.Task):
     sample = luigi.Parameter()
     block_size = luigi.Parameter()
     context = luigi.Parameter()
-    db_host = luigi.Parameter()
-    db_name = luigi.Parameter()
     num_workers = luigi.Parameter()
     fragments_in_xy = luigi.BoolParameter()
     mask_fragments = luigi.BoolParameter()
@@ -146,7 +143,7 @@ class ExtractFragments(luigi.Task):
 
     def run(self):
 
-        output_base = os.path.join(self.output_dir(), self.sample) + '_extract'
+        output_base = os.path.join(self.output_dir(), self.sample) + '_extract_agglomerate'
         log_out = output_base + '.out'
         log_err = output_base + '.err'
 
@@ -165,7 +162,7 @@ class ExtractFragments(luigi.Task):
                 'sample': self.sample,
                 'block_size': self.block_size,
                 'context': self.context,
-                'db_host': self.db_host,
+                'db_host': db_host,
                 'db_name': db_name,
                 'num_workers': 40,
                 'fragments_in_xy': self.fragments_in_xy,
@@ -180,100 +177,8 @@ class ExtractFragments(luigi.Task):
                     'run_lsf',
                     '-c', '2',
                     '-g', '1',
-                    '-d', 'funkey/lsd:v0.3',
-                    'python -u extract_fragments_blockwise.py ' + config_filename
-                ], stdout=o, stderr=e)                )
-
-    def output(self):
-
-        db_name = get_db_name(
-            self.experiment,
-            self.setup,
-            self.iteration,
-            self.sample)
-
-        return [
-                N5DatasetTarget(self.output_filename(), 'volumes/fragments'),
-                MongoDbCollectionTarget(db_name, self.db_host, 'nodes')
-            ]
-
-    def output_dir(self):
-        return os.path.join(base_dir, self.experiment, '03_predict', self.setup, str(self.iteration))
-
-    def output_filename(self):
-        return os.path.join(self.output_dir(), self.sample)
-
-class Agglomerate(luigi.Task):
-
-    experiment = luigi.Parameter()
-    setup = luigi.Parameter()
-    iteration = luigi.IntParameter()
-    sample = luigi.Parameter()
-    block_size = luigi.Parameter()
-    context = luigi.Parameter()
-    db_host = luigi.Parameter()
-    db_name = luigi.Parameter()
-    num_workers = luigi.Parameter()
-    fragments_in_xy = luigi.BoolParameter()
-    mask_fragments = luigi.BoolParameter()
-    # TODO: add merge function
-
-    def requires(self):
-
-        db_name = get_db_name(
-            self.experiment,
-            self.setup,
-            self.iteration,
-            self.sample)
-
-        return ExtractFragments(
-            self.experiment,
-            self.setup,
-            self.iteration,
-            self.sample,
-            self.block_size,
-            self.context,
-            self.db_host,
-            db_name,
-            self.num_workers,
-            self.fragments_in_xy,
-            self.mask_fragments)
-
-    def run(self):
-
-        output_base = os.path.join(self.output_dir(), self.sample) + '_agglomerate'
-        log_out = output_base + '.out'
-        log_err = output_base + '.err'
-
-        db_name = get_db_name(
-            self.experiment,
-            self.setup,
-            self.iteration,
-            self.sample)
-
-        config_filename = output_base + '.json'
-        with open(config_filename, 'w') as f:
-            json.dump({
-                'experiment': self.experiment,
-                'setup': self.setup,
-                'iteration': self.iteration,
-                'sample': self.sample,
-                'block_size': self.block_size,
-                'context': self.context,
-                'db_host': self.db_host,
-                'db_name': db_name,
-                'num_workers': 40
-            }, f)
-
-        os.chdir(os.path.join(base_dir, 'scripts'))
-        with open(log_out, 'w') as o:
-            with open(log_err, 'w') as e:
-                check_call([
-                    'run_lsf',
-                    '-c', '2',
-                    '-g', '1',
-                    '-d', 'funkey/lsd:v0.3'
-                    'python -u agglomerate_blockwise.py ' + config_filename
+                    '-d', 'sheridana/lsd:v0.4test ',
+                    'python -u extract_agglomerate_blockwise.py ' + config_filename
                 ], stdout=o, stderr=e)
 
     def output(self):
@@ -284,13 +189,14 @@ class Agglomerate(luigi.Task):
             self.iteration,
             self.sample)
 
-        return MongoDbCollectionTarget(db_name, self.db_host, 'edges')
+        return MongoDbCollectionTarget(db_name, db_host, 'edges')
 
     def output_dir(self):
         return os.path.join(base_dir, self.experiment, '03_predict', self.setup, str(self.iteration))
 
     def output_filename(self):
         return os.path.join(self.output_dir(), self.sample)
+
 
 class Evaluate(luigi.Task):
 
@@ -300,7 +206,6 @@ class Evaluate(luigi.Task):
     sample = luigi.Parameter()
     block_size = luigi.Parameter()
     context = luigi.Parameter()
-    db_host = luigi.Parameter()
     num_workers = luigi.Parameter()
     fragments_in_xy = luigi.BoolParameter()
     mask_fragments = luigi.BoolParameter()
@@ -315,15 +220,13 @@ class Evaluate(luigi.Task):
             self.iteration,
             self.sample)
 
-        return Agglomerate(
+        return ExtractAgglomerate(
             self.experiment,
             self.setup,
             self.iteration,
             self.sample,
             self.block_size,
             self.context,
-            self.db_host,
-            db_name,
             self.num_workers,
             self.fragments_in_xy,
             self.mask_fragments)
@@ -348,7 +251,7 @@ class Evaluate(luigi.Task):
                 'iteration': self.iteration,
                 'sample': self.sample,
                 'border_threshold': self.border_threshold,
-                'db_host': self.db_host,
+                'db_host': db_host,
                 'db_name': db_name,
                 'thresholds': self.thresholds
             }, f)
@@ -358,7 +261,7 @@ class Evaluate(luigi.Task):
             with open(log_err, 'w') as e:
                 check_call([
                     'run_docker',
-                    '-d', 'sheridana/lsd:v0.4test',
+                    '-d', 'sheridana/lsd:v0.4test ',
                     'python -u evaluate.py ' + config_filename
                 ], stdout=o, stderr=e)
 
@@ -373,7 +276,7 @@ class Evaluate(luigi.Task):
             self.iteration,
             self.sample)
 
-        return MongoDbCollectionTarget(db_name, self.db_host, 'scores') # TODO: store in global scores DB
+        return MongoDbCollectionTarget(db_name, db_host, 'scores') # TODO: store in global scores DB
 
 class EvaluateCombinations(luigi.task.WrapperTask):
 
