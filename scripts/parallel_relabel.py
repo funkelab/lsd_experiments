4881b5127ed750aeb32a326e61f5ab2c0cd23e9e
Diptodip Deb <dr.diptodip@gmail.com>
2018-10-10 18:27:27 -0400

    Do nothing if no relevant fragments in block

--- scripts/parallel_relabel.py
+++ scripts/parallel_relabel.py
@@ -19,17 +19,20 @@ def relabel_in_block(block, segmentation_ds, fragments_map, ignore=[0]):
     fragments = fragments[np.isin(fragments, ignore, invert=True)]
 
     # shift fragment values to potentially save memory when relabeling
-    min_fragment = fragments.min()
-    assert min_fragment > 0
-    offset = fragments.dtype.type(min_fragment - 1)
-    volume[np.isin(volume, ignore, invert=True)] -= offset
-    shifted_fragments = fragments - offset
+    if len(fragments) > 0:
+        min_fragment = fragments.min()
+        assert min_fragment > 0
+        offset = fragments.dtype.type(min_fragment - 1)
+        volume[np.isin(volume, ignore, invert=True)] -= offset
+        shifted_fragments = fragments - offset
 
-    components = np.array([fragments_map[fragment] for fragment in fragments], dtype=fragments.dtype)
-    logging.debug("Restricted fragments map to {0} elements".format(len(components)))
-    volume = lsd.labels.replace_values(volume, shifted_fragments, components)
-    logging.debug("Writing relabeled block to segmentation volume")
-    segmentation_ds[block.read_roi] = volume
+        components = np.array([fragments_map[fragment] for fragment in fragments], dtype=fragments.dtype)
+        logging.debug("Restricted fragments map to {0} elements".format(len(components)))
+        volume = lsd.labels.replace_values(volume, shifted_fragments, components)
+        logging.debug("Writing relabeled block to segmentation volume")
+        segmentation_ds[block.read_roi] = volume
+    else:
+        logging.debug("Block {0} contains no relevant fragments".format(block.block_id))
 
 def parallel_relabel(
         components,
