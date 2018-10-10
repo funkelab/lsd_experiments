a5aa4cc1dd85277454e18b9bf821b91dac00c5c4
Diptodip Deb <dr.diptodip@gmail.com>
2018-10-10 18:18:40 -0400

    Fix bug that mapped only ID 1

--- scripts/parallel_renumber.py
+++ scripts/parallel_renumber.py
@@ -21,23 +21,26 @@ def unique_in_block(block, segmentation_ds, id_registry, ignore=[0]):
     id_registry.update(dict_entries)
 
 def renumber_in_block(block, segmentation_ds, id_map, ignore=[0]):
-    logging.debug("Relabeling in {0}".format(block.read_roi))
+    logging.debug("Renumbering in {0}".format(block.read_roi))
     volume = segmentation_ds[block.read_roi].data
     old_ids = np.unique(volume)
     old_ids = old_ids[np.isin(old_ids, ignore, invert=True)]
 
     # shift ID values to potentially save memory when relabeling
-    min_id = old_ids.min()
-    assert min_id > 0
-    offset = old_ids.dtype.type(min_id - 1)
-    volume[np.isin(volume, ignore, invert=True)] -= offset
-    shifted_old_ids = old_ids - offset
+    if len(old_ids > 0):
+        min_id = old_ids.min()
+        assert min_id > 0
+        offset = old_ids.dtype.type(min_id - 1)
+        volume[np.isin(volume, ignore, invert=True)] -= offset
+        shifted_old_ids = old_ids - offset
 
-    new_ids = [id_map[old_id] for old_id in old_ids]
-    logging.debug("Restricted ID map to {0} elements".format(len(new_ids)))
-    volume = lsd.labels.replace_values(volume, shifted_old_ids, new_ids)
-    logging.debug("Writing relabeled block to segmentation volume")
-    segmentation_ds[block.read_roi] = volume
+        new_ids = np.array([id_map[old_id] for old_id in old_ids], dtype=old_ids.dtype)
+        logging.debug("Restricted ID map to {0} elements".format(len(new_ids)))
+        volume = lsd.labels.replace_values(volume, shifted_old_ids, new_ids)
+        logging.debug("Writing relabeled block to segmentation volume")
+        segmentation_ds[block.read_roi] = volume
+    else:
+        logging.debug("Block {0} contains no relevant IDs".format(block.block_id))
 
 def parallel_renumber(
         segmentation_ds,
@@ -52,6 +55,7 @@ def parallel_renumber(
  
     m = Manager()
     id_registry = m.dict()
+    print(len(id_registry))
 
     logging.info("Starting unique ID registration tasks")
     for i in range(retry + 1):
@@ -71,8 +75,11 @@ def parallel_renumber(
 
         if i < retry:
             logging.error("parallel ID registration failed, retrying %d/%d", i + 1, retry)
+    
+    print(len(id_registry))
 
-    id_map = {old_id: i+1 for i, old_id in enumerate(id_registry.values())}
+    id_map = {old_id: i+1 for i, old_id in enumerate(id_registry)}
+    print(len(id_map))
 
     for i in range(retry + 1):
         if daisy.run_blockwise(
