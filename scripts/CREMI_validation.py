502f5ccf36f69fd6bdc5bc67113a55b1a7f94f9b
Jan Funke <jfunke@iri.upc.edu>
2018-10-29 15:26:58 -0400

    Update CREMI validation script

--- scripts/CREMI_validation.py
+++ scripts/CREMI_validation.py
@@ -10,15 +10,32 @@ if __name__ == '__main__':
 
     combinations = {
         'experiment': 'cremi',
-        'setups': ['setup01', 'setup04'],
-        'iterations': [500000],
+        'setups': [
+            'setup01',
+            'setup04',
+            'setup05',
+            'setup08',
+            'setup09',
+            'setup12',
+            'setup22',
+            'setup23',
+            'setup34',
+            'setup35',
+            'setup40'
+        ],
+        'iterations': [300000, 400000, 500000],
         'samples': ['testing/sample_C_padded_20160501.aligned.filled.cropped.62:153.n5'],
         'block_size': [2240, 2048, 2048],
         'context': [320, 256, 256],
         'fragments_in_xy': True,
         'epsilon_agglomerate': 0,
         'mask_fragments': True,
-        'merge_functions': ['hist_quant_25_initmax', 'hist_quant_50_initmax', 'hist_quant_75_initmax'],
+        'merge_functions': [
+            'hist_quant_25_initmax',
+            'hist_quant_50_initmax',
+            'hist_quant_75_initmax',
+            'mean'
+        ],
         'border_threshold': 25,
         'thresholds_minmax': [0, 1],
         'thresholds_step': 0.1
