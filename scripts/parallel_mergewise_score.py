0731d02782ae30017ab62cc2da81bb03f0fecd9b
Diptodip Deb <d2d.tar.gz@gmail.com>
2018-11-20 13:24:16 -0500

    Calculate entropies in function and use Counter dicts

--- scripts/parallel_mergewise_score.py
+++ scripts/parallel_mergewise_score.py
@@ -83,6 +83,38 @@ def create_chunk_slices(total_size, chunk_size):
     logging.debug("Creating chunks of size {0} for {1} elements".format(chunk_size, total_size))
     return [slice(i, min(i+chunk_size, total_size)) for i in range(0, total_size, chunk_size)]
 
+def segmentation_entropies(contingencies,
+                           seg_counts,
+                           gt_seg_counts,
+                           total,
+                           chunk_size,
+                           num_workers):
+    dask.config.set(scheduler='processes')
+    contingencies_chunks = create_chunk_slices(len(contingencies), chunk_size)
+    seg_counts_chunks = create_chunk_slices(len(seg_counts), chunk_size)
+    gt_seg_counts_chunks = create_chunk_slices(len(gt_seg_counts), chunk_size)
+    contingency_list = list(contingencies.values())
+    seg_count_list = list(seg_counts.values())
+    gt_seg_count_list = list(gt_seg_counts.values())
+
+    delayed_H_contingencies = [
+            dask.delayed(entropy_in_chunk)(
+                contingency_list[c],
+                total) for c in contingencies_chunks]
+    delayed_H_seg = [
+            dask.delayed(entropy_in_chunk)(
+                seg_count_list[c],
+                total) for c in seg_counts_chunks]
+    delayed_H_gt_seg = [
+            dask.delayed(entropy_in_chunk)(
+                gt_seg_count_list[c],
+                total) for c in gt_seg_counts_chunks]
+
+    H_contingencies = dask.delayed(sum)(delayed_H_contingencies).compute(num_workers=num_workers)
+    H_seg = dask.delayed(sum)(delayed_H_seg).compute(num_workers=num_workers)
+    H_gt_seg = dask.delayed(sum)(delayed_H_gt_seg).compute(num_workers=num_workers)
+    return (H_contingencies, H_seg, H_gt_seg)
+
 def parallel_mergewise_score(rag,
                              fragments_file,
                              fragments_dataset,
@@ -107,32 +139,16 @@ def parallel_mergewise_score(rag,
                                      retry)
     
     logging.info("Calculating entropies for fragments")
-
-    dask.config.set(scheduler='processes')
-    contingencies_chunks = create_chunk_slices(contingencies.nnz, chunk_size)
-    seg_counts_chunks = create_chunk_slices(seg_counts.nnz, chunk_size)
-    gt_seg_counts_chunks = create_chunk_slices(gt_seg_counts.nnz, chunk_size)
-
-    delayed_H_contingencies = [
-            dask.delayed(entropy_in_chunk)(
-                contingencies.data[c],
-                total) for c in contingencies_chunks]
-    delayed_H_seg = [
-            dask.delayed(entropy_in_chunk)(
-                seg_counts.data[c],
-                total) for c in seg_counts_chunks]
-    delayed_H_gt_seg = [
-            dask.delayed(entropy_in_chunk)(
-                gt_seg_counts.data[c],
-                total) for c in gt_seg_counts_chunks]
-
-    H_contingencies = dask.delayed(sum)(delayed_H_contingencies).compute(num_workers=num_workers)
-    H_seg = dask.delayed(sum)(delayed_H_seg).compute(num_workers=num_workers)
-    H_gt_seg = dask.delayed(sum)(delayed_H_gt_seg).compute(num_workers=num_workers)
+    (H_contingencies, H_seg, H_gt_seg) = segmentation_entropies(contingencies,
+                                                                fragment_counts,
+                                                                gt_seg_counts,
+                                                                total,
+                                                                chunk_size,
+                                                                num_workers)
     voi_split = H_contingencies - H_gt_seg
     voi_merge = H_contingencies - H_seg
-    logging.info("Fragment VOI split: {0} VOI merge: {1}".format(voi_split, voi_merge))
     results = [(voi_split, voi_merge)]
+    logging.info("Fragment VOI split: {0} VOI merge: {1}".format(voi_split, voi_merge))
 
     for threshold in thresholds:
         logging.info("Calculating connected components for threshold {0}".format(threshold))
