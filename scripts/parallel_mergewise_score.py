8a24e91f3d34072663503ae9aaa2ae7063a772e6
Diptodip Deb <d2d.tar.gz@gmail.com>
2018-11-20 23:28:06 -0500

    Merge chunks of components and fix typos

--- scripts/parallel_mergewise_score.py
+++ scripts/parallel_mergewise_score.py
@@ -39,11 +39,25 @@ def _delta_entropy_col(counter, columns, total, new_column):
     Returns change in entropy resulting from a merge of columns of ``counter``
     specified in ``columns``.
     """
-    removed_columns = _removed_columns(counter, columns)
-    merged_column = _merge_columns(counter, columns, new_column)
-    entropy_to_remove = entropy_in_chunk(list(removed_columns.values()), total)
-    entropy_to_add = entropy_in_chunk(list(merged_column.values()), total)
+    removed_columns = np.array(list(_removed_columns(counter, columns).values()),
+                               dtype=np.float64)
+    merged_column = np.array(list(_merge_columns(counter, columns, new_column).values()),
+                             dtype=np.float64)
+    removed_columns = removed_columns[np.nonzero(removed_columns)]
+    merged_column = merged_column[np.nonzero(merged_column)]
+    entropy_to_remove = entropy_in_chunk(removed_columns,
+                                         total)
+    entropy_to_add = entropy_in_chunk(merged_column,
+                                      total)
     return entropy_to_add - entropy_to_remove
+
+def delta_entropy_in_chunk(counter, components_in_chunk, chunk, num_components, total):
+    logging.info("Calculating entropy update for {0} merged components".format(len(components_in_chunk)))
+    delta = 0
+    new_columns = range(*chunk.indices(num_components))
+    for i, component in enumerate(components_in_chunk):
+        delta += _delta_entropy_col(counter, component, total, new_columns[i])
+    return delta
 
 def delta_entropy(contingencies,
                   fragment_counts,
@@ -56,19 +70,21 @@ def delta_entropy(contingencies,
     fragments of each component in ``components``. This computation is
     performed in parallel with ``num_workers`` processes.
     """
-    logging.info("Calculating entropy update for {0} merged components".format(len(components)))
-    delayed_delta_H_contingencies = [dask.delayed(_delta_entropy_col)(contingencies,
-                                                                      c,
-                                                                      total,
-                                                                      i+1)
-                                     for i, c in enumerate(components)]
-    delayed_delta_H_seg = [dask.delayed(_delta_entropy_col)(fragment_counts,
-                                                            c,
-                                                            total,
-                                                            i+1)
-                                     for i, c in enumerate(components)]
-    delta_H_contingincies = dask.delayed(sum)(delayed_delta_H_contingencies).compute(num_workers=num_workers)
-    delta_H_seg = dask.delayed(sum)(delayed_delta_H_contingencies).compute(num_workers=num_workers)
+    components_chunks = create_chunk_slices(len(components), 1000)
+    delayed_delta_H_contingencies = [dask.delayed(delta_entropy_in_chunk)(contingencies,
+                                                                          components[chunk],
+                                                                          chunk,
+                                                                          len(components),
+                                                                          total)
+                                     for chunk in components_chunks]
+    delayed_delta_H_seg = [dask.delayed(delta_entropy_in_chunk)(fragment_counts,
+                                                                components[chunk],
+                                                                chunk,
+                                                                len(components),
+                                                                total)
+                                     for chunk in components_chunks]
+    delta_H_contingencies = dask.delayed(sum)(delayed_delta_H_contingencies).compute(num_workers=num_workers)
+    delta_H_seg = dask.delayed(sum)(delayed_delta_H_seg).compute(num_workers=num_workers)
     return (delta_H_contingencies, delta_H_seg)
 
 def entropy_in_chunk(chunk, total):
@@ -149,9 +165,9 @@ def parallel_mergewise_score(rag,
                                                                 num_workers)
     voi_split = H_contingencies - H_gt_seg
     voi_merge = H_contingencies - H_seg
-    results = [(voi_split, voi_merge)]
     logging.info("Fragment VOI split: {0} VOI merge: {1}".format(voi_split, voi_merge))
 
+    results = [] 
     for threshold in thresholds:
         logging.info("Calculating connected components for threshold {0}".format(threshold))
         components = rag.get_connected_components(threshold)
@@ -160,6 +176,9 @@ def parallel_mergewise_score(rag,
                                                              components,
                                                              total,
                                                              num_workers)
+        logging.info("Calculated updates are {0} and {1}".format(delta_H_contingencies,
+                                                                 delta_H_seg))
         voi_split = (H_contingencies + delta_H_contingencies) - H_gt_seg
         voi_merge = (H_contingencies + delta_H_contingencies) - (H_seg + delta_H_seg)
         results.append((voi_split, voi_merge))
+    return results
