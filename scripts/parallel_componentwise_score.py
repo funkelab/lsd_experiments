cc196813975940df6b30a377bf5a3b046c009f77
Diptodip Deb <d2d.tar.gz@gmail.com>
2018-11-29 11:12:52 -0500

    Avoid unnecessary entropy calculations

--- scripts/parallel_componentwise_score.py
+++ scripts/parallel_componentwise_score.py
@@ -30,37 +30,21 @@ def create_chunk_slices(total_size, chunk_size):
     logger.debug("Creating chunks of size {0} for {1} elements".format(chunk_size, total_size))
     return [slice(i, min(i+chunk_size, total_size)) for i in range(0, total_size, chunk_size)]
 
-def fragment_entropies(contingencies,
-                       seg_counts,
-                       gt_seg_counts,
+def gt_seg_entropies(gt_seg_counts,
                        total,
                        chunk_size,
                        num_workers):
     dask.config.set(scheduler='processes')
-    contingencies_chunks = create_chunk_slices(len(contingencies), chunk_size)
-    seg_counts_chunks = create_chunk_slices(len(seg_counts), chunk_size)
     gt_seg_counts_chunks = create_chunk_slices(len(gt_seg_counts), chunk_size)
-    contingency_list = list(contingencies.values())
-    seg_count_list = list(seg_counts.values())
     gt_seg_count_list = list(gt_seg_counts.values())
 
-    delayed_H_contingencies = [
-            dask.delayed(entropy_in_chunk)(
-                contingency_list[c],
-                total) for c in contingencies_chunks]
-    delayed_H_seg = [
-            dask.delayed(entropy_in_chunk)(
-                seg_count_list[c],
-                total) for c in seg_counts_chunks]
     delayed_H_gt_seg = [
             dask.delayed(entropy_in_chunk)(
                 gt_seg_count_list[c],
                 total) for c in gt_seg_counts_chunks]
 
-    H_contingencies = dask.delayed(sum)(delayed_H_contingencies).compute(num_workers=num_workers)
-    H_seg = dask.delayed(sum)(delayed_H_seg).compute(num_workers=num_workers)
     H_gt_seg = dask.delayed(sum)(delayed_H_gt_seg).compute(num_workers=num_workers)
-    return (H_contingencies, H_seg, H_gt_seg)
+    return H_gt_seg
 
 def segmentation_entropies(contingencies,
                            seg_counts,
@@ -110,13 +94,11 @@ def parallel_componentwise_score(rag,
                                          num_workers,
                                          retry)
     
-    logger.info("Calculating entropies for fragments")
-    (H_contingencies, H_seg, H_gt_seg) = fragment_entropies(contingencies,
-                                                            fragment_counts,
-                                                            gt_seg_counts,
-                                                            total,
-                                                            chunk_size,
-                                                            num_workers)
+    logger.info("Calculating entropies for ground truth")
+    H_gt_seg = gt_seg_entropies(gt_seg_counts,
+                                  total,
+                                  chunk_size,
+                                  num_workers)
 
     results = [] 
     for threshold in thresholds:
