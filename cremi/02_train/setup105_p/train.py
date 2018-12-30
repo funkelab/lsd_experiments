a47a732a09698a89d51b0fe80ae8dc9dec0e2b1d
Arlo Sheridan <sheridana@janelia.hhmi.org>
2018-12-30 15:04:32 -0500

    Add copies of autocontext paper setups using only 6 data samples

---
+++ cremi/02_train/setup105_p/train.py
@@ -0,0 +1,265 @@
+from __future__ import print_function
+import os
+import sys
+import logging
+from gunpowder import *
+from gunpowder.tensorflow import *
+from mala.gunpowder import AddLocalShapeDescriptor
+import malis
+import math
+import json
+import tensorflow as tf
+import numpy as np
+
+logging.basicConfig(level=logging.INFO)
+
+data_dir = '../../01_data/glia_mask/'
+artifacts_dir = '../../01_data/training/'
+
+samples = [
+    'sample_0',
+    'sample_1',
+    'sample_2',
+    'sample_A',
+    'sample_B',
+    'sample_C'
+]
+
+setup_dir = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(setup_dir, 'config.json'), 'r') as f:
+    aff_net_config = json.load(f)
+
+experiment_dir = os.path.join(setup_dir, '..', '..')
+lsd_setup_dir = os.path.realpath(os.path.join(
+    experiment_dir,
+    '02_train',
+    aff_net_config['lsd_setup']))
+
+neighborhood = [[-1, 0, 0], [0, -1, 0], [0, 0, -1]]
+
+def train_until(max_iteration):
+
+    if tf.train.latest_checkpoint('.'):
+        trained_until = int(tf.train.latest_checkpoint('.').split('_')[-1])
+    else:
+        trained_until = 0
+    if trained_until >= max_iteration:
+        return
+
+    with open('train_net.json', 'r') as f:
+        context_config = json.load(f)
+        
+    with open('train_lsd_net.json', 'r') as f:
+        sd_config = json.load(f)
+
+    raw = ArrayKey('RAW')
+    raw_cropped = ArrayKey('RAW_CROPPED')
+    labels = ArrayKey('GT_LABELS')
+    labels_mask = ArrayKey('GT_LABELS_MASK')
+    artifacts = ArrayKey('ARTIFACTS')
+    artifacts_mask = ArrayKey('ARTIFACTS_MASK')
+    pretrained_lsd = ArrayKey('PRETRAINED_LSD')
+    embedding = ArrayKey('PREDICTED_EMBEDDING')
+    affs = ArrayKey('PREDICTED_AFFS')
+    gt_embedding = ArrayKey('GT_EMBEDDING')
+    gt_embedding_scale = ArrayKey('GT_EMBEDDING_SCALE')
+    gt_affs = ArrayKey('GT_AFFINITIES')
+    gt_affs_mask = ArrayKey('GT_AFFINITIES_MASK')
+    gt_affs_scale = ArrayKey('GT_AFFINITIES_SCALE')
+    affs_gradient = ArrayKey('AFFS_GRADIENT')
+
+    voxel_size = Coordinate((40,4,4))
+    sd_input_size = Coordinate(sd_config['input_shape'])*voxel_size
+    context_input_size = Coordinate(context_config['input_shape'])*voxel_size
+    pretrained_lsd_size = Coordinate(context_config['input_shape'])*voxel_size
+    output_size = Coordinate(context_config['output_shape'])*voxel_size
+
+    request = BatchRequest()
+    request.add(raw, sd_input_size)
+    request.add(raw_cropped, context_input_size)
+    request.add(labels, output_size)
+    request.add(labels_mask, output_size)
+    request.add(pretrained_lsd, pretrained_lsd_size)
+    request.add(gt_embedding, output_size)
+    request.add(gt_embedding_scale, output_size)
+    request.add(gt_affs, output_size)
+    request.add(gt_affs_mask, output_size)
+    request.add(gt_affs_scale, output_size)
+
+    snapshot_request = BatchRequest({
+        embedding: request[gt_embedding],
+        affs: request[gt_affs],
+        affs_gradient: request[gt_affs]
+    })
+
+    data_sources = tuple(
+        Hdf5Source(
+            os.path.join(data_dir, sample + '.hdf'),
+            datasets = {
+                raw: 'volumes/raw',
+                raw_cropped: 'volumes/raw',
+                labels: 'volumes/labels/neuron_ids',
+                labels_mask: 'volumes/labels/mask',
+            },
+            array_specs = {
+                raw: ArraySpec(interpolatable=True),
+                raw_cropped: ArraySpec(interpolatable=True),
+                labels: ArraySpec(interpolatable=False),
+                labels_mask: ArraySpec(interpolatable=False)
+            }
+        ) +
+        Normalize(raw) +
+        Normalize(raw_cropped) +
+        Pad(raw, None) +
+        Pad(raw_cropped, None) +
+        RandomLocation() +
+        Reject(mask=labels_mask)
+        for sample in samples
+    )
+
+    artifact_source = (
+        Hdf5Source(
+            os.path.join(artifacts_dir, 'sample_ABC_padded_20160501.defects.hdf'),
+            datasets = {
+                artifacts: 'defect_sections/raw',
+                artifacts_mask: 'defect_sections/mask',
+            },
+            array_specs = {
+                artifacts: ArraySpec(
+                    voxel_size=(40, 4, 4),
+                    interpolatable=True),
+                artifacts_mask: ArraySpec(
+                    voxel_size=(40, 4, 4),
+                    interpolatable=True),
+            }
+        ) +
+        RandomLocation(min_masked=0.05, mask=artifacts_mask) +
+        Normalize(artifacts) +
+        IntensityAugment(artifacts, 0.9, 1.1, -0.1, 0.1, z_section_wise=True) +
+        ElasticAugment(
+            control_point_spacing=[4,40,40],
+            jitter_sigma=[0,2,2],
+            rotation_interval=[0,math.pi/2.0],
+            subsample=8) +
+        SimpleAugment(transpose_only=[1, 2])
+    )
+
+    train_pipeline = (
+        data_sources +
+        RandomProvider() +
+        ElasticAugment(
+            control_point_spacing=[4,40,40],
+            jitter_sigma=[0,2,2],
+            rotation_interval=[0,math.pi/2.0],
+            prob_slip=0.05,
+            prob_shift=0.05,
+            max_misalign=10,
+            subsample=8) +
+        SimpleAugment(transpose_only=[1, 2]) +
+        IntensityAugment(raw, 0.9, 1.1, -0.1, 0.1, z_section_wise=True) +
+        IntensityAugment(raw_cropped, 0.9, 1.1, -0.1, 0.1, z_section_wise=True) +
+        GrowBoundary(labels, labels_mask, steps=1, only_xy=True) +
+        AddLocalShapeDescriptor(
+            labels,
+            gt_embedding,
+            mask=gt_embedding_scale,
+            sigma=80,
+            downsample=2) +
+        AddAffinities(
+            neighborhood,
+            labels=labels,
+            affinities=gt_affs,
+            labels_mask=labels_mask,
+            affinities_mask=gt_affs_mask) +
+        BalanceLabels(
+            gt_affs,
+            gt_affs_scale,
+            gt_affs_mask) +
+        DefectAugment(
+            raw,
+            prob_missing=0.03, 
+            prob_low_contrast=0.01,
+            prob_artifact=0.03,
+            artifact_source=artifact_source,
+            artifacts=artifacts,
+            artifacts_mask=artifacts_mask,
+            contrast_scale=0.5, 
+            axis=0) +
+        DefectAugment(
+            raw_cropped,
+            prob_missing=0.03, 
+            prob_low_contrast=0.01,
+            prob_artifact=0.03,
+            artifact_source=artifact_source,
+            artifacts=artifacts,
+            artifacts_mask=artifacts_mask,
+            contrast_scale=0.5, 
+            axis=0) +
+        IntensityScaleShift(raw, 2,-1) +
+        IntensityScaleShift(raw_cropped, 2,-1) +
+        PreCache(
+            cache_size=40,
+            num_workers=10) +
+        Predict(
+            checkpoint=os.path.join(
+                lsd_setup_dir,
+                'train_lsd_net_checkpoint_%d'%aff_net_config['lsd_iteration']),
+            graph='train_lsd_net.meta',
+            inputs={
+                sd_config['raw']: raw
+            },
+            outputs={
+                sd_config['embedding']: pretrained_lsd
+            }) +
+        Train(
+            'train_net',
+            optimizer=context_config['optimizer'],
+            loss=context_config['loss'],
+            inputs={
+                context_config['raw']: raw_cropped,
+                context_config['pretrained_lsd']: pretrained_lsd,
+                context_config['gt_embedding']: gt_embedding,
+                context_config['loss_weights_embedding']: gt_embedding_scale,
+                context_config['gt_affs']: gt_affs,
+                context_config['loss_weights_affs']: gt_affs_scale,
+            },
+            outputs={
+                context_config['embedding']: embedding,
+                context_config['affs']: affs
+            },
+            gradients={
+                context_config['affs']: affs_gradient
+            },
+            summary=context_config['summary'],
+            log_dir='log',
+            save_every=10000) +
+        IntensityScaleShift(raw, 0.5, 0.5) +
+        IntensityScaleShift(raw_cropped, 0.5, 0.5) +
+        Snapshot({
+                raw_cropped: 'volumes/raw',
+                labels: 'volumes/labels/neuron_ids',
+                gt_embedding: 'volumes/labels/gt_embedding',
+                embedding: 'volumes/labels/pred_embedding',
+                gt_affs: 'volumes/labels/gt_affinities',
+                affs: 'volumes/labels/pred_affinities',
+                affs_gradient: 'volumes/affs_gradient'
+            },
+            dataset_dtypes={
+                labels: np.uint64
+            },
+            every=1000,
+            output_filename='batch_{iteration}.hdf',
+            additional_request=snapshot_request) +
+        PrintProfilingStats(every=10)
+    )
+
+    print("Starting training...")
+    with build(train_pipeline) as b:
+        for i in range(max_iteration - trained_until):
+            b.request_batch(request)
+    print("Training finished")
+
+if __name__ == "__main__":
+    iteration = int(sys.argv[1])
+    train_until(iteration)
---
+++ cremi/02_train/setup107_p/mknet.py
@@ -0,0 +1,139 @@
+import mala
+from mala.networks.unet import crop_zyx
+import tensorflow as tf
+import json
+
+def create_affs_network(input_shape, output_shape, name):
+
+    tf.reset_default_graph()
+
+    raw = tf.placeholder(tf.float32, shape=input_shape)
+    raw_batched = tf.reshape(raw, (1, 1) + input_shape)
+
+    unet, _, _ = mala.networks.unet(raw_batched, 12, 5, [[1,3,3],[1,3,3],[3,3,3]])
+
+    affs_batched, _ = mala.networks.conv_pass(
+        unet,
+        kernel_sizes=[1],
+        num_fmaps=3,
+        activation='sigmoid',
+        name='affs')
+
+    affs_batched = crop_zyx(affs_batched, (1, 3) + output_shape)
+    affs = tf.reshape(affs_batched, (3,) + output_shape)
+
+    print("input shape : %s"%(input_shape,))
+    print("output shape: %s"%(output_shape,))
+
+    tf.train.export_meta_graph(filename=name + '.meta')
+
+    config = {
+        'raw': raw.name,
+        'affs': affs.name,
+        'input_shape': input_shape,
+        'output_shape': output_shape}
+    with open(name + '.json', 'w') as f:
+        json.dump(config, f)
+
+def create_affs2_network(input_shape, intermediate_shape, expected_output_shape, name):
+
+    tf.reset_default_graph()
+
+    raw = tf.placeholder(tf.float32, shape=input_shape)
+    raw_batched = tf.reshape(raw, (1, 1) + input_shape)
+    raw_in = tf.reshape(raw_batched, input_shape)
+    raw_batched = crop_zyx(raw_batched, (1, 1) + intermediate_shape)
+
+    raw_cropped = tf.reshape(raw_batched, intermediate_shape)
+
+    pretrained_affs = tf.placeholder(tf.float32, shape=(3,) + intermediate_shape)
+    pretrained_affs_batched = tf.reshape(pretrained_affs, (1, 3) + intermediate_shape)
+
+    concat_input = tf.concat([raw_batched, pretrained_affs_batched], axis=1)
+
+    unet, _, _ = mala.networks.unet(concat_input, 12, 5, [[1,3,3],[1,3,3],[3,3,3]])
+
+    affs_batched, _ = mala.networks.conv_pass(
+        unet,
+        kernel_sizes=[1],
+        num_fmaps=3,
+        activation='sigmoid',
+        name='affs')
+    affs = tf.squeeze(affs_batched, axis=0)
+
+    output_shape = tuple(affs.get_shape().as_list()[1:])
+    assert expected_output_shape == output_shape, "%s !=%s"%(expected_output_shape, output_shape)
+
+    gt_affs = tf.placeholder(tf.float32, shape=(3,) + output_shape)
+    affs_loss_weights = tf.placeholder(tf.float32, shape=(3,) + output_shape)
+    
+    loss = tf.losses.mean_squared_error(
+        gt_affs,
+        affs,
+        affs_loss_weights)
+
+    summary = tf.summary.scalar('setup107_eucl_loss', loss)
+
+    opt = tf.train.AdamOptimizer(
+        learning_rate=0.5e-4,
+        beta1=0.95,
+        beta2=0.999,
+        epsilon=1e-8)
+    optimizer = opt.minimize(loss)
+
+    # output_shape = output_shape[1:]
+    print("input shape : %s"%(intermediate_shape,))
+    print("output shape: %s"%(output_shape,))
+
+    tf.train.export_meta_graph(filename=name + '.meta')
+
+    config = {
+        'raw': raw.name,
+        'raw_cropped': raw_cropped.name,
+        'raw_in': raw_in.name,
+        'pretrained_affs': pretrained_affs.name,
+        'affs': affs.name,
+        'gt_affs': gt_affs.name,
+        'affs_loss_weights': affs_loss_weights.name,
+        'loss': loss.name,
+        'optimizer': optimizer.name,
+        'input_shape': intermediate_shape,
+        'output_shape': output_shape,
+        'summary': summary.name,
+        }
+    with open(name + '.json', 'w') as f:
+        json.dump(config, f)
+
+def create_config(input_shape, output_shape, num_dims, name):
+
+    config = {
+        'input_shape': input_shape,
+        'output_shape': output_shape,
+        'out_dims': num_dims,
+        'out_dtype': 'uint8',
+        'affs_setup': 'setup71_p',
+        'affs_iteration': 400000
+        }
+    with open(name + '.json', 'w') as f:
+        json.dump(config, f)
+
+if __name__ == "__main__":
+
+    train_input_shape = (120, 484, 484)
+    train_intermediate_shape = (84, 268, 268)
+    train_output_shape = (48, 56, 56)
+
+    create_affs_network(train_input_shape, train_intermediate_shape, 'train_affs_net')
+    create_affs2_network(train_intermediate_shape, train_intermediate_shape, train_output_shape, 'train_net')
+
+    z=0
+    xy=0
+
+    test_input_shape = (120, 484, 484)
+    test_intermediate_shape = (84, 268, 268)
+    test_output_shape = (48, 56, 56)
+
+    create_affs2_network(test_input_shape, test_intermediate_shape, test_output_shape, 'test_net')
+
+    create_config(test_input_shape, test_output_shape, 3, 'config')
+
