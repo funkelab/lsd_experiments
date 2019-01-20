bd895d2d4cfb2e8f0d998f375374bfabf2ec59e0
Jan Funke <funkej@janelia.hhmi.org>
2019-01-19 19:02:50 -0500

    Simplify parallel_relabel.py

--- scripts/parallel_relabel.py
+++ scripts/parallel_relabel.py
@@ -1,42 +1,40 @@
-import dask
 import daisy
 import lsd
-import os
-import sys
-import shutil
 import logging
-from concurrent.futures  import ProcessPoolExecutor as Pool
-from multiprocessing import Process, sharedctypes
 import numpy as np
 
-logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-def relabel_in_block(block, segmentation_ds, fragments_ds, fragments_map, ignore=[0]):
-    logging.debug("Relabeling in {0}".format(block.read_roi))
+def relabel_in_block(block, segmentation_ds, fragments_ds, fragments_map):
+
+    logger.info("Relabeling in %s", block.read_roi)
     volume = fragments_ds[block.read_roi].to_ndarray()
     fragments = np.unique(volume)
-    # fragments = fragments[np.isin(fragments, ignore, invert=True)]
+    segments = np.array([
+        fragments_map.get(fragment, fragment)
+        for fragment in fragments
+    ], dtype=fragments.dtype)
+
+    if len(fragments) == 0:
+        logger.warn("Block %s contains no relevant fragments", block.block_id)
+        return
 
     # shift fragment values to potentially save memory when relabeling
-    if len(fragments) > 0:
-        min_fragment = fragments.min()
-        offset = 0
-        if min_fragment > 0:
-            offset = fragments.dtype.type(min_fragment - 1)
-        # volume[np.isin(volume, ignore, invert=True)] -= offset
+    min_fragment = fragments.min()
+    offset = 0
+    if min_fragment > 0:
+        offset = fragments.dtype.type(min_fragment - 1)
         volume -= offset
-        shifted_fragments = fragments - offset
+        fragments -= offset
 
-        components = np.array([fragments_map[fragment] for fragment in fragments], dtype=fragments.dtype)
-        logging.debug("Restricted fragments map to {0} elements".format(len(components)))
-        volume = lsd.labels.replace_values(volume, shifted_fragments, components)
-        logging.debug("Writing relabeled block to segmentation volume")
-        segmentation_ds[block.read_roi] = volume
-    else:
-        logging.debug("Block {0} contains no relevant fragments".format(block.block_id))
+    logger.debug("Mapping fragments to %d segments", len(segments))
+    volume = lsd.labels.replace_values(volume, fragments, segments)
+
+    logger.debug("Writing relabeled block to segmentation volume")
+    segmentation_ds[block.read_roi] = volume
 
 def parallel_relabel(
-        components,
+        fragments_map,
         fragments_file,
         fragments_dataset,
         total_roi,
@@ -45,18 +43,20 @@ def parallel_relabel(
         seg_dataset,
         num_workers,
         retry):
-    
+
     read_roi = daisy.Roi((0,)*3, block_size)
     write_roi = daisy.Roi((0,)*3, block_size)
-    
-    logging.info("Constructing temporary segmentation dataset")
+
+    logger.info("Preparing segmentation dataset...")
     fragments_ds = daisy.open_ds(fragments_file, fragments_dataset)
-    segmentation_ds = daisy.prepare_ds(seg_file, seg_dataset, total_roi, fragments_ds.voxel_size, dtype=np.uint32)
+    segmentation_ds = daisy.prepare_ds(
+        seg_file,
+        seg_dataset,
+        total_roi,
+        voxel_size=fragments_ds.voxel_size,
+        dtype=np.uint64,
+        write_roi=write_roi)
 
-    logging.info("Constructing dictionary from fragments to components")
-    fragments_map = {fragment: i+1 for i, component in enumerate(components) for fragment in component}
-
-    logging.info("Starting relabeling tasks for {0} components".format(len(components)))
     for i in range(retry + 1):
         if daisy.run_blockwise(
             total_roi,
@@ -74,4 +74,4 @@ def parallel_relabel(
                 break
 
         if i < retry:
-            logging.error("parallel relabel failed, retrying %d/%d", i + 1, retry)
+            logger.error("parallel relabel failed, retrying %d/%d", i + 1, retry)
