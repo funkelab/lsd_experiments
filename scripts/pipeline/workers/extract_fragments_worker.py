8232f22250f7b36b6b5e9a1074af12c9e4b291bd
Arlo Sheridan <sheridana@janelia.hhmi.org>
2019-11-16 15:00:31 -0500

    Add num_voxels var in watershed worker, remove globals

--- scripts/pipeline/workers/extract_fragments_worker.py
+++ scripts/pipeline/workers/extract_fragments_worker.py
@@ -8,6 +8,7 @@ import time
 
 logging.basicConfig(level=logging.INFO)
 
+
 def extract_fragments_worker(input_config):
 
     logging.info(sys.argv)
@@ -17,8 +18,17 @@ def extract_fragments_worker(input_config):
 
     logging.info(config)
 
-    for key in config:
-        globals()['%s' % key] = config[key]
+    affs_file = config['affs_file']
+    affs_dataset = config['affs_dataset']
+    fragments_file = config['fragments_file']
+    fragments_dataset = config['fragments_dataset']
+    db_name = config['db_name']
+    db_host = config['db_host']
+    queue = config['queue']
+    num_voxels_in_block = config['num_voxels_in_block']
+    fragments_in_xy=config['fragments_in_xy']
+    epsilon_agglomerate=config['epsilon_agglomerate']
+    filter_fragments=config['filter_fragments']
 
     logging.info("Reading affs from %s", affs_file)
     affs = daisy.open_ds(affs_file, affs_dataset, mode='r')
@@ -29,10 +39,13 @@ def extract_fragments_worker(input_config):
         fragments_dataset,
         mode='r+')
 
-    if mask_file:
+    if 'mask_file' in config:
 
-        logging.info("Reading mask from %s", mask_file)
-        mask = daisy.open_ds(mask_file, mask_dataset, mode='r')
+        logging.info("Reading mask from %s", config['mask_file'])
+        mask = daisy.open_ds(
+            config['mask_file'],
+            config['mask_dataset'],
+            mode='r')
 
     else:
 
@@ -65,27 +78,34 @@ def extract_fragments_worker(input_config):
 
         start = time.time()
 
-        logging.info("block read roi begin: %s",block.read_roi.get_begin())
-        logging.info("block read roi shape: %s",block.read_roi.get_shape())
-        logging.info("block write roi begin: %s",block.write_roi.get_begin())
-        logging.info("block write roi shape: %s",block.write_roi.get_shape())
+        logging.info("block read roi begin: %s", block.read_roi.get_begin())
+        logging.info("block read roi shape: %s", block.read_roi.get_shape())
+        logging.info("block write roi begin: %s", block.write_roi.get_begin())
+        logging.info("block write roi shape: %s", block.write_roi.get_shape())
 
         lsd.watershed_in_block(
             affs,
             block,
             rag_provider,
             fragments,
-            fragments_in_xy,
-            epsilon_agglomerate=epsilon_agglomerate,
+            num_voxels_in_block=num_voxels_in_block,
             mask=mask,
+            fragments_in_xy=fragments_in_xy,
+            epsilon_agglomerate=epsilon_agglomerate,
             filter_fragments=filter_fragments)
 
         document = {
             'num_cpus': 5,
             'queue': queue,
             'block_id': block.block_id,
-            'read_roi': (block.read_roi.get_begin(), block.read_roi.get_shape()),
-            'write_roi': (block.write_roi.get_begin(), block.write_roi.get_shape()),
+            'read_roi': (
+                block.read_roi.get_begin(),
+                block.read_roi.get_shape()
+            ),
+            'write_roi': (
+                block.write_roi.get_begin(),
+                block.write_roi.get_shape()
+            ),
             'start': start,
             'duration': time.time() - start
         }
